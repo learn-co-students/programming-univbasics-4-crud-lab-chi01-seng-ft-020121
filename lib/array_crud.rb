a4bfb7403464f6e3377be5f2e656eceb9df2c077
Val_1 S_z <csa.aiden@gmail.com>
2021-01-23 00:51:16 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,41 @@
 def create_an_empty_array
-  
+  []  
 end
 
 def create_an_array
-  
-end
-
-def add_element_to_end_of_array(array, element)
-  
+  ["dog", "cat", "fish", "owl"]
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def add_element_to_end_of_array(array, element)
+  array << element
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
+end
+def remove_element_from_end_of_array(array)
+  array.pop
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retreive_element_from_index(array, index_number)
+  array[index_number]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retreive_first_element_from_array(array)
+  array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retreive_last_element_from_array(array)
+  x = array.count
+  array[x-1]
 end
 
-def update_element_from_index(array, index_number, element)
 
+def update_element_from_index = ["Red", "Black", "Yelo", "Sin", "Moon", "Eyes"]
+update_element_from_index[4] = "Keystone"
+end
 end
